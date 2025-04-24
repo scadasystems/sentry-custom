7bdc79bb96513247442fc314b9070f9e409b26ba
LulzM <yunmo159789@gmail.com>
2025-04-24 16:46:25 +0900

    DB 경로 변경

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -1,14 +1,41 @@
-echo "${_group}Building and tagging Docker images ..."
+echo "${_group}Creating volumes for persistent storage ..."
 
-echo ""
-# Build any service that provides the image sentry-self-hosted-local first,
-# as it is used as the base image for sentry-cleanup-self-hosted-local.
-$dcb --force-rm web
-# Build each other service individually to localize potential failures better.
-for service in $($dc config --services); do
-  $dcb --force-rm "$service"
-done
-echo ""
-echo "Docker images built."
+mkdir -p ./db/{sentry-clickhouse,sentry-data,sentry-kafka,sentry-postgres,sentry-redis,sentry-symbolicator}
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-clickhouse \
+  --opt o=bind \
+  --name=sentry-clickhouse)."
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-data \
+  --opt o=bind \
+  --name=sentry-data)."
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-kafka \
+  --opt o=bind \
+  --name=sentry-kafka)."
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-postgres \
+  --opt o=bind \
+  --name=sentry-postgres)."
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-redis \
+  --opt o=bind \
+  --name=sentry-redis)."
+
+echo "Created $(docker volume create --driver local \
+  --opt type=none \
+  --opt device=$(pwd)/db/sentry-symbolicator \
+  --opt o=bind \
+  --name=sentry-symbolicator)."
 
 echo "${_endgroup}"
